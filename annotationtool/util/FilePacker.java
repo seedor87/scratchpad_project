b6fa6a92031f454170439038c2e465e19fae0554
armstr <armstr@robilabs312.rowanads.rowan.edu>
2017-08-14 20:02:07 -0400

    Created FilePacker class, for reading/writing zip files.

---
+++ annotationtool/util/FilePacker.java
@@ -0,0 +1,84 @@
+package util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
+
+public class FilePacker {
+	public static void main(String[] args) {
+		System.out.println(retrieveFromZip("ReadZip.coolguy", "test"));
+	}
+	
+	public static String retrieveFromZip(String zipPath, String fileContents) {
+		String fileName = "";
+		try {
+			ZipFile zip = new ZipFile(zipPath);
+			Enumeration entries = zip.entries();
+			while(entries.hasMoreElements()) {
+				ZipEntry entry = (ZipEntry) entries.nextElement();
+				if(entry.getName().startsWith(fileContents, 0)) {
+					InputStream in = zip.getInputStream(entry);
+					fileName = "/tmp/" + entry.getName();
+					if(new File(fileName).exists()) {
+						new File(fileName).delete();
+					}
+					Files.copy(in, Paths.get(fileName));
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			return fileName;
+		}
+	}
+	
+	public static void createZip(String zipName, ArrayList<String> fileNames) {
+		try {
+			FileOutputStream fOut = new FileOutputStream(zipName + ".annot");
+			ZipOutputStream zOut = new ZipOutputStream(fOut);
+			
+			for(String fileName : fileNames) {
+				addToZip(fileName, zOut);
+			}
+			
+			zOut.close();
+			fOut.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void addToZip(String fileName, ZipOutputStream zos) throws FileNotFoundException, IOException {
+
+		System.out.println("Writing '" + fileName + "' to zip file");
+
+		File file = new File(fileName);
+		FileInputStream fis = new FileInputStream(file);
+		ZipEntry zipEntry = new ZipEntry(fileName);
+		zos.putNextEntry(zipEntry);
+
+		byte[] bytes = new byte[1024];
+		int length;
+		while ((length = fis.read(bytes)) >= 0) {
+			zos.write(bytes, 0, length);
+		}
+
+		zos.closeEntry();
+		fis.close();
+	}
+
+	
+}
